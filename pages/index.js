e2ba1f24f82a69e0b9563e8edc93f995952a324a
Zansxd <134784573+eggspm2@users.noreply.github.com>
2024-09-26 20:17:50 +0700

    index.js

--- pages/index.js
+++ pages/index.js
@@ -15,19 +15,19 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>balxzzy | Spotify Music</title>
+        <title>SanzMD | Spotify Music</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
-        <link rel="stylesheet" href="https://nyimpen.vercel.app/component/spotfy.css" />
+        <link rel="stylesheet" href="https://camo.githubusercontent.com/84e61db3c78714dc2ef3b62401024754b77e9beacb5ffbe0fa3eb200cd944982/68747470733a2f2f74656c656772612e70682f66696c652f6538623433366335663839353838663035656432622e6a7067" />
       </Head>
       <div className="container">
         <header>
           <img src="https://nyimpen.vercel.app/component/logoku.png" alt="Profile Picture" className="profile-pic" />
           <h1 className="nama">balxzzy</h1>
           <p className="lokasi">
-            <i className="fas fa-globe-asia"></i> Kalimantan Timur, Samarinda
+            <i className="fas fa-globe-asia"></i> Sumate5 Selatan, Sekayu
           </p>
-          <p className="bio">”destroyer.”</p>
+          <p className="bio">”Destroyer.”</p>
         </header>
 
         <section className="spotify-section">
@@ -39,7 +39,7 @@ export default function Home() {
               </div>
             )}
             <iframe
-              src="https://open.spotify.com/embed/playlist/6e5psrTK5oyi4xFRCk4MRx"
+              src="https://open.spotify.com/playlist/49CD1JayymAVNK64zrQYbE"
               frameBorder="0"
               allowTransparency="true"
               allow="encrypted-media"
@@ -49,16 +49,16 @@ export default function Home() {
         </section>
 
         <div className="social-icons">
-          <a href="https://github.com/balxz" target="_blank" className="social-link" rel="noopener noreferrer">
+          <a href="github.com/eggspm2" target="_blank" className="social-link" rel="noopener noreferrer">
             <i className="fab fa-github"></i>
           </a>
-          <a href="https://wa.me/6281276400345?text=hi+balxzzy" target="_blank" className="social-link" rel="noopener noreferrer">
+          <a href="wa.me/6283168629450" target="_blank" className="social-link" rel="noopener noreferrer">
             <i className="fab fa-whatsapp"></i>
           </a>
-          <a href="https://instagram.com/iqstore78" target="_blank" className="social-link" rel="noopener noreferrer">
+          <a href="https://www.instagram.com/reyheresz" target="_blank" className="social-link" rel="noopener noreferrer">
             <i className="fab fa-instagram"></i>
           </a>
-          <a href="https://api.balxzzy.xyz" target="_blank" className="social-link" rel="noopener noreferrer">
+          <a href="https://api.sanz.vercel" target="_blank" className="social-link" rel="noopener noreferrer">
             <i className="fas fa-arrow-up-right-from-square"></i>
           </a>
         </div>
